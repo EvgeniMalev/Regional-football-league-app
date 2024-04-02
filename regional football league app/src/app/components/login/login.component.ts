dd7651e8648ea046c77fc8539df93424c3e244b2
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-04-02 22:54:24 +0300

    Create login.component.ts

---
+++ regional football league app/src/app/components/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService) {}
+
+  login() {
+    this.authService.login(this.email, this.password)
+      .then(() => {
+      })
+      .catch(error => {
+        this.errorMessage = error.message;
+      });
+  }
+}
