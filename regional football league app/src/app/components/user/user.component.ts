25dea21ef3dbdcc3383f793a2fb8e7f19a2443f4
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-04-02 23:12:34 +0300

    Create user.component.ts

---
+++ regional football league app/src/app/components/user/user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+  user = this.authService.currentUser; 
+
+  constructor(private authService: AuthService) {}
+}
