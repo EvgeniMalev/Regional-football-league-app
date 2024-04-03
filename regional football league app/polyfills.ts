482f44ef495bbb4db40cd31145f8bbda208561e6
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-04-03 23:19:15 +0300

    Create polyfills.ts

---
+++ regional football league app/polyfills.ts
@@ -0,0 +1,11 @@
+import 'core-js/es';
+import 'core-js/stable';
+import 'core-js/features/array';
+import 'core-js/features/object';
+import 'core-js/features/string';
+
+import 'web-animations-js';  
+
+import 'intersection-observer';  
+
+import 'zone.js/dist/zone';
